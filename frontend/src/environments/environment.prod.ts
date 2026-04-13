d854c390bfbe243e89c2e9884115b7e6dbfc6c83
paulomunozdev <paulomunozdev@github.com>
2026-04-13 14:01:50 -0400

    fix: update API URL to new App Service hostname

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://api-solicitudesapp-dyd2f7gcb2b6fva4.brazilsouth-01.azurewebsites.net/api',
+  apiUrl: 'https://api-solicitudesapp.azurewebsites.net/api',
   googleClientId: '946422420661-6dtj5kg0kcbs62ah4b45fmdggau665me.apps.googleusercontent.com',
 };
